a78882bc8a1fb421dc3658b86bdc889da6759621
Hernan G. Gonzalez <camus.sonico@gmail.com>
2023-12-20 15:57:42 -0300

    Parse Arguments

---
+++ src/args.rs
@@ -0,0 +1,31 @@
+use anyhow::{Context, Error, Result};
+use std::env;
+
+#[derive(Debug)]
+pub enum Command {
+    Info,
+}
+
+impl TryFrom<String> for Command {
+    type Error = Error;
+
+    fn try_from(value: String) -> Result<Self> {
+        match value.as_str() {
+            ".dbinfo" => Ok(Command::Info),
+            e => Err(anyhow::anyhow!("Not a command: {e}")),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Args {
+    pub filename: String,
+    pub cmds: Vec<Command>,
+}
+
+pub fn build() -> Result<Args> {
+    let mut args = env::args().skip(1);
+    let filename = args.next().context("Missing filename")?;
+    let cmds = args.flat_map(Command::try_from).collect();
+    Ok(Args { filename, cmds })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
-use anyhow::{bail, Result};
+mod args;
+
+use anyhow::Result;
+use args::Command;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> Result<()> {
-    // Parse arguments
-    let args = std::env::args().collect::<Vec<_>>();
-    match args.len() {
-        0 | 1 => bail!("Missing <database path> and <command>"),
-        2 => bail!("Missing <command>"),
-        _ => {}
-    }
+    // Commands
+    let args = args::build()?;
 
     // Parse command and act accordingly
-    let command = &args[2];
-    match command.as_str() {
-        ".dbinfo" => {
-            let mut file = File::open(&args[1])?;
-            let mut header = [0; 100];
-            file.read_exact(&mut header)?;
+    for cmd in args.cmds {
+        match cmd {
+            Command::Info => {
+                let mut file = File::open(&args.filename)?;
+                let mut header = [0; 100];
+                file.read_exact(&mut header)?;
 
-            // The page size is stored at the 16th byte offset, using 2 bytes in big-endian order
-            #[allow(unused_variables)]
-            let page_size = u16::from_be_bytes([header[16], header[17]]);
+                let page_size = u16::from_be_bytes([header[16], header[17]]);
 
-            println!("database page size: {}", page_size);
+                println!("database page size: {}", page_size);
+            }
         }
-        _ => bail!("Missing or invalid command passed: {}", command),
     }
 
     Ok(())
